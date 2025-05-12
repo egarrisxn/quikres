7b41b8cf2ccb4a0b71541f0a1ee0452c46afa388
egarrisxn <egarrisxn@gmail.com>
2025-05-12 15:44:00 -0400

    error

--- lib/constants.ts
+++ lib/constants.ts
@@ -8,9 +8,7 @@ export const BASE_URL =
 export const SITE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : (process?.env?.NEXT_PUBLIC_SITE_URL ??
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-      "https://quikres.vercel.app");
+    : (process?.env?.NEXT_PUBLIC_SITE_URL ?? "https://quikres.vercel.app");
 
 export const SITE_DATA: {
   title: string;
